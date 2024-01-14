5871e55f13195f6a0425a92b48557ffed0c8a08d
pandacase <2227119810@qq.com>
2024-01-14 16:51:43 +0800

    Update kv_master_server.cc

--- src/kv_master_server.cc
+++ src/kv_master_server.cc
@@ -36,14 +36,17 @@
 
 #endif
 
+using grpc::Channel;
+using grpc::ClientContext;
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
 using distributedKV::Greeter;
-using distributedKV::HelloReply;
 using distributedKV::HelloRequest;
+using distributedKV::HelloReply;
 
 using distributedKV::kvMethods;
 using distributedKV::KVRequest;
@@ -55,6 +58,7 @@ using distributedKV::survivalList;
 
 // Default port (master)
 ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
+ABSL_FLAG(std::string, addr, "localhost", "Server address");
 
 // Logic and data behind the server's behavior.
 class GreeterServiceImpl final : public Greeter::Service {
@@ -66,24 +70,234 @@ class GreeterServiceImpl final : public Greeter::Service {
   }
 };
 
-// The list recording that which worker is active.
-std::unordered_set<int> survival_list;
-// The pointer point to the next worker : impl for load balancing
-int next_worker;
+//!< The list recording that which worker is active.
+std::unordered_set<uint16_t> survival_list;
 
+//! @brief Register Server End <--- Worker Server
+//! 
+//! @details Get register request from a new setup worker.
 class workerRegisterServiceImpl final : public workerRegister::Service {
+  Status Register(ServerContext* context, const workerSetup* request,
+                  survivalList* response) {
+    // Parse segment from request.
+    const std::string& message = request->message();
+    uint16_t port = request->port();
 
+    // Record this worker.
+    survival_list.insert(port); 
+
+    // Set the response.
+    response->set_message("Register Successfully!");
+    for (const auto& port : survival_list) {
+      response->add_ports(port);
+    }
+
+    return Status::OK;
+  }
 };
 
-class kvMethodsServiceImpl final : public kvMethods::Service {
-  // Status Get()
+
+//! @brief Register Client End ---> Worker Server
+//! 
+//! @details Broadcast latest survival list to all worker.
+class workerRegisterClient {
+  // bool  
 };
 
 
+//! @brief KV Client End ---> Worker Server
+//! 
+//! @details It's actually the same as client's Client End.
+class kvMethodsClient {
+ public:
+  kvMethodsClient(std::shared_ptr<Channel> channel)
+      : stub_(kvMethods::NewStub(channel)) {}
+
+  //! @brief Get the value from remoteDB with key.
+  //! 
+  //! @param key : to query.
+  //! @return KVResponse : the response
+  KVResponse Get(const std::string& key) {
+    KVRequest request;
+    request.set_key(key);
+
+    KVResponse response;
+    
+    ClientContext context;
+    
+    // actual rpc
+    Status status = stub_->Get(&context, request, &response);
+
+    if (status.ok()) {
+      std:: cout << "Message: " << response.message() << std::endl;
+      return response;
+    } else {
+      std::cout << "Code "<< status.error_code() << ": " 
+                << status.error_message() << std::endl;
+      response.set_error(true);
+      return response;
+    }
+  }
+
+  //! @brief Put the new value to the remoteDB with key,
+  //!        can be `update` or `insert`.
+  //! 
+  //! @param key : to update.
+  //! @param value : new value
+  //! @return KVResponse : The reponse loaded with new value.
+  KVResponse Put(const std::string& key, const std::string& value) {
+    KVRequest request;
+    request.set_key(key);
+    request.set_value(value);
+
+    KVResponse response;
+    
+    ClientContext context;
+    
+    // actual rpc
+    Status status = stub_->Put(&context, request, &response);
+
+    if (status.ok()) {
+      std:: cout << "Message: " << response.message() << std::endl;
+      return response;
+    } else {
+      std::cout << "Code "<< status.error_code() << ": " 
+                << status.error_message() << std::endl;
+      response.set_error(true);
+      return response;
+    }
+  }
+
+  //! @brief Delete the entry on the remoteDB with key.
+  //! 
+  //! @param key : to delete.
+  //! @return KVResponse : the response
+  KVResponse Del(const std::string& key) {
+    KVRequest request;
+    request.set_key(key);
+
+    KVResponse response;
+    
+    ClientContext context;
+    
+    // actual rpc
+    Status status = stub_->Del(&context, request, &response);
+
+    if (status.ok()) {
+      std:: cout << "Message: " << response.message() << std::endl;
+      return response;
+    } else {
+      std::cout << "Code "<< status.error_code() << ": " 
+                << status.error_message() << std::endl;
+      response.set_error(true);
+      return response;
+    }
+  }
+
+ private:
+  std::unique_ptr<kvMethods::Stub> stub_;
+};
+
+
+//! @brief KV Server End <--- Client
+//! 
+//! @details Will forward the request ---> Worker
+class kvMethodsMasterServiceImpl final : public kvMethods::Service {
+  //!< The pointer point to the next worker : impl for load balancing
+  int next_worker = 0;
+  //!< Key Lock
+  std::unordered_set<std::string> locks;
+
+  //! @brief Get the Worker Port object : roll pooling
+  uint16_t getWorkerPort() {
+    int cnt = (next_worker++) % survival_list.size();
+    for (const auto& port: survival_list) {
+      if (cnt == 0) {
+        return port;
+      }
+      cnt -= 1;
+    }
+    return 0;
+  }
+
+  //! @brief Get the Worker Socket object
+  std::string getWorkerSocket() {
+    uint16_t port = getWorkerPort();
+    std::string socket = absl::GetFlag(FLAGS_addr) + ":" + std::to_string(port);
+    return socket;
+  }
+
+  Status Get(ServerContext* context, const KVRequest* reqeust,
+            KVResponse* response) {
+    // Parse segment from request.
+    const std::string& key = reqeust->key();
+    const std::string& value = reqeust->value();
+
+    // Forward the request to worker server
+    kvMethodsClient methods = kvMethodsClient(
+      grpc::CreateChannel(getWorkerSocket(), grpc::InsecureChannelCredentials()));
+    *response = methods.Get(key);
+
+    return Status::OK;
+  }
+
+  Status Put(ServerContext* context, const KVRequest* reqeust,
+            KVResponse* response) {
+    // Parse segment from request.
+    const std::string& key = reqeust->key();
+    const std::string& value = reqeust->value();
+
+    // Check if the key is locked.
+    if (locks.count(key) != 0) {
+      return Status::CANCELLED;
+    } 
+    // Get the lock.
+    locks.insert(key);
+
+    // Forward the request to worker server
+    kvMethodsClient methods = kvMethodsClient(
+      grpc::CreateChannel(getWorkerSocket(), grpc::InsecureChannelCredentials()));
+    *response = methods.Put(key, value);
+
+    // Release the lock.
+    locks.erase(key);
+
+    return Status::OK;
+  }
+
+  Status Del(ServerContext* context, const KVRequest* reqeust,
+            KVResponse* response) {
+    // Parse segment from request.
+    const std::string& key = reqeust->key();
+    const std::string& value = reqeust->value();
+
+    // Check if the key is locked.
+    if (locks.count(key) != 0) {
+      return Status::CANCELLED;
+    } 
+    // Get the lock.
+    locks.insert(key);
+
+    // Forward the request to worker server
+    kvMethodsClient methods = kvMethodsClient(
+      grpc::CreateChannel(getWorkerSocket(), grpc::InsecureChannelCredentials()));
+    *response = methods.Del(key);
+
+    // Release the lock.
+    locks.erase(key);
+
+    return Status::OK;
+  }
+};
+
+
+//! @brief Server Runtime.
+//! 
+//! @param port : working port
 void RunServer(uint16_t port) {
   std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
   GreeterServiceImpl greeter_service;
-  kvMethodsServiceImpl kvMethods_service;
+  kvMethodsMasterServiceImpl kvMethods_service;
   workerRegisterServiceImpl workerRegister_service;
 
   grpc::EnableDefaultHealthCheckService(true);
@@ -104,6 +318,7 @@ void RunServer(uint16_t port) {
   // responsible for shutting down the server for this call to ever return.
   server->Wait();
 }
+
 
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
